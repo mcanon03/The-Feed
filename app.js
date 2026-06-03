dd0417f7e59de80daa9dee9daa308a3bbcc1561e
Marilou Canon <marilou.canon@gmail.com>
2026-06-02 23:14:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-
+const sprintData = [
+  { sprint: "Sprint 29", committed: 25, completed: 21 },
+  { sprint: "Sprint 30", committed: 21.5, completed: 20.5 },
+  { sprint: "Sprint 31", committed: 16, completed: 16 }, // Scope change/carryover
+  { sprint: "Sprint 32", committed: 16, completed: 13 },
+  { sprint: "Sprint 33", committed: 14, completed: 16 }
+];
